0785ab8f9820f2a808157363e42d570a2c0cafac
CszMurray <calvin.t.murray@hotmail.co.uk>
2014-10-07 17:55:50 +0100

    Basic client-server, Maven and JavaScript eclipse projects

---
+++ SLIP-WebClient/public/hello.js
@@ -0,0 +1,25 @@
+angular.module('appM', []).
+controller('Hello', function($scope, $http, $timeout) {
+
+  // Function to get the data
+  $scope.getData = function(){
+    $http.get('http://localhost:8080/greeting')
+      .success(function(data, status, headers, config) {
+
+    	  $scope.greeting = data;
+      console.log('Fetched data!');
+    });
+  };
+
+  // Function to replicate setInterval using $timeout service.
+  $scope.intervalFunction = function(){
+    $timeout(function() {
+      $scope.getData();
+      $scope.intervalFunction();
+    }, 1000)
+  };
+
+  // Kick off the interval
+  $scope.intervalFunction();
+
+});
---
+++ SLIP-WebServer/src/main/java/hello/Greeting.java
@@ -0,0 +1,29 @@
+package hello;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+    private Date time;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    
+    public String getTime(){
+    	time = new Date();
+    	return new SimpleDateFormat("hh:mm:ss").format(time);
+    }
+}
