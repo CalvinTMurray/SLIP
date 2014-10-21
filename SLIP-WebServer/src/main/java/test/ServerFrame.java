da26e4b6d41d14b8495a666a8b351e2cf524cdaa
Chimwemwe Kadzuwa <ck.1@hotmail.co.uk>
2014-10-21 18:39:27 +0100

    New ServerFrame and ServerPayload classes

---
+++ SLIP-WebServer/src/main/java/test/ServerFrame.java
@@ -0,0 +1,51 @@
+package test;
+
+import java.util.List;
+
+public class ServerFrame {
+
+	private int sessionID;
+	private int payloadSize;
+	private List<ServerPayload> payloads;
+
+	public int getSessionID() {
+		return sessionID;
+	}
+
+	public int getPayloadSize() {
+		return payloadSize;
+	}
+
+	public List<ServerPayload> getPayloads() {
+		return payloads;
+	}
+
+	public void setSessionID(int sessionID) {
+		this.sessionID = sessionID;
+	}
+
+	public void setPayloadSize(int payloadSize) {
+		this.payloadSize = payloadSize;
+	}
+
+	public void setPayloads(List<ServerPayload> payloads) {
+		this.payloads = payloads;
+	}
+
+	@Override
+	public String toString() {
+
+		String str = "-----------------\nSession ID:[" + sessionID + "]" + "\n"
+				+ "PayloadSize:[" + payloadSize + "]\n-----------------\n";
+
+		for (int i = 0; i < payloadSize; i++) {
+			str += "Payload: " + i + " ";
+			str += payloads.get(i).toString();
+			str += "\n";
+		}
+
+		return str;
+
+	}
+
+}
--- SLIP-WebServer/src/main/java/test/TestController.java
+++ SLIP-WebServer/src/main/java/test/TestController.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+	private ArrayList<ServerPayload> payloads = new ArrayList<ServerPayload>();
 
 	@RequestMapping(method = RequestMethod.POST, value = "/test", headers = { "Content-type=application/json" }, produces = { "application/json" })
 	public @ResponseBody String test(@RequestBody String payload) {
@@ -18,22 +23,34 @@ public class TestController {
 
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "/pframe", headers = { "Content-type=application/json" }, produces = { "application/json" })
-	public @ResponseBody String pframe(@RequestBody ProspeckzFrame frame) {
+	@RequestMapping(method = RequestMethod.POST, value = "/sframe", headers = { "Content-type=application/json" }, produces = { "application/json" })
+	public @ResponseBody String sframe(@RequestBody ServerFrame frame) {
 
-		System.out.println(frame);
+		System.out.println(frame.toString());
 
-		return "Posting frame handled";
+		List<ServerPayload> receivedPayloads = frame.getPayloads();
+		int n = receivedPayloads.size();
+
+		for (int i = 0; i < n; i++) {
+			payloads.add(receivedPayloads.get(i));
+		}
+
+		return "Post for frame handled";
 
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "/ppayload", headers = { "Content-type=application/json" }, produces = { "application/json" })
-	public @ResponseBody String ppayload(@RequestBody ProspeckzPayload payload) {
+	@RequestMapping(method = RequestMethod.POST, value = "/spayload", headers = { "Content-type=application/json" }, produces = { "application/json" })
+	public @ResponseBody String spayload(@RequestBody ServerPayload payload) {
 
 		System.out.println(payload);
 
 		return "Posting payload handled";
 
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/payloads")
+	public List<ServerPayload> payloads() {
+		return payloads;
 	}
 
 }
