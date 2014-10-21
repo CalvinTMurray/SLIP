494290b87ad1b894f038fe3dcbc712759b7eb80e
CszMurray <calvin.t.murray@hotmail.co.uk>
2014-10-21 20:35:09 +0100

    serverManager

--- SLIP-WebServer/src/main/java/dataAccessLayer/ServerPayloadMapper.java
+++ SLIP-WebServer/src/main/java/dataAccessLayer/ServerPayloadMapper.java
@@ -5,20 +5,18 @@ import java.sql.SQLException;
 
 import org.springframework.jdbc.core.RowMapper;
 
-import test.ProspeckzPayload;
+import test.ServerPayload;
 
-public class ProspeckzPayloadMapper implements RowMapper<ProspeckzPayload>{
+public class ServerPayloadMapper implements RowMapper<ServerPayload>{
 
-	@Override
-	public ProspeckzPayload mapRow(ResultSet rs, int rowNum) throws SQLException {
-		System.out.println("I'm getting called yaya!");
-		ProspeckzPayload payload = new ProspeckzPayload();
-		payload.setReceiver_one(rs.getInt("ReceiverOneDist"));
-		payload.setReceiver_two(rs.getInt("ReceiverTwoDist"));
-		payload.setReceiver_three(rs.getInt("ReceiverThreeDist"));
-		payload.setReceiver_four(rs.getInt("ReceiverFourDist"));
-		
-		return payload;
-	}
+  @Override
+  public ServerPayload mapRow(ResultSet rs, int rowNum) throws SQLException {
+    ServerPayload payload = new ServerPayload();
+    
+    payload.setX(rs.getInt("xPosition"));
+    payload.setY(rs.getInt("yPosition"));
+    
+    return payload;
+  }
 
 }
