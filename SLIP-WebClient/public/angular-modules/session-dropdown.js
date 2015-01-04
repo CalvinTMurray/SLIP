5ada8747fc7533feb1f32038d993430ee1c93a56
CszMurray <calvin.t.murray@hotmail.co.uk>
2015-01-04 17:19:14 +0000

    Initial selected session update chart data When a page refreshes each charts data is requested automatically This resolves #1

--- SLIP-WebClient/public/angular-modules/session-dropdown.js
+++ SLIP-WebClient/public/angular-modules/session-dropdown.js
@@ -1,33 +1,38 @@
 angular.module('mtsModule', [])
 
-.controller('SessionController', ['$http', '$scope', function($http, $scope) {
-	
-	$scope.selectedChart = 1;
-	
-	$scope.getSessions = function () {
-		$http.get('http://localhost:8080/all-sessions').
-		success(function(data, status, headers, config) {
-			$scope.sessionIDs = data;
-			$scope.selectedSession = data[0].sessionID;
-			
-			console.log("dropdown is being selected")
-			
-//			$scope.updateData($scope.selectedSession);
-			
-		}).error(function(data, status, headers, config) {
-			console.log('Error on getting session IDs')
-		});
-	}
-	
-	$scope.getSessions();
-	
-	$scope.updateData = function (sessionID) {
-		$scope.selectedSession = sessionID;
-		console.log("Updating the data " + sessionID)
-		
-		setPoints($scope.selectedSession);
-		setDistancePoints($scope.selectedSession);
-		
-	}
-}])
+	.controller('SessionController', ['$http', '$scope', function($http, $scope) {
+
+		$scope.selectedChart = 1;
+
+		$scope.selectedSession = null;
+
+		$scope.getSessions = function () {
+			$http.get('http://192.168.0.15:8080/all-sessions')
+				.success(function(data, status, headers, config) {
+					$scope.sessionIDs = data;
+
+					if ($scope.selectedSession === null) {
+						$scope.selectedSession = data[0].sessionID;
+						$scope.updateData($scope.selectedSession);
+					}
+
+					console.log("dropdown is being selected");
+
+				})
+				.error(function(data, status, headers, config) {
+					console.log('Error on getting session IDs')
+				});
+		};
+
+		$scope.getSessions();
+
+		$scope.updateData = function (sessionID) {
+			$scope.selectedSession = sessionID;
+			console.log("Updating the data " + sessionID);
+
+			setPoints($scope.selectedSession);
+			setDistancePoints($scope.selectedSession);
+
+		}
+	}]);
 
