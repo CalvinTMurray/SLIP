7e7ee4e58670f4963233bc1b62507c56487c657e
CszMurray <calvin.t.murray@hotmail.co.uk>
2015-01-07 17:46:20 +0000

    Deleted test class and changed HeatmapPlot back to original

--- SLIP-WebServer/src/main/java/charts/HeatmapPlot.java
+++ SLIP-WebServer/src/main/java/charts/HeatmapPlot.java
@@ -32,9 +32,9 @@ public class HeatmapPlot extends AbstractChart<int[]> {
     protected void createChart(long sessionID) {
         System.out.println("\nCREATING HEATMAP PLOT");
 
-        int numberOfxGrids = 14;
-        int numberOfyGrids = 14;
-        double width_and_height = 5; // Each grid square is 10 x 10;
+        int numberOfxGrids = 7;
+        int numberOfyGrids = 7;
+        double width_and_height = 10; // Each grid square is 10 x 10;
 
         int[][] grid = new int[numberOfxGrids][numberOfyGrids];
 
